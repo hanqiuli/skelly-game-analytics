767512127fe056cf0397a674e0abf73ae51b7fd4
hanqiuli <hanqiulicai@gmail.com>
2023-11-30 15:52:27 +0100

    Edited existing endpoints for handling reading/writing json, added docs

--- web_app.py
+++ web_app.py
@@ -80,58 +80,239 @@ def view_numbers():
     return render_template('view_numbers.html', numbers=numbers)
 
 
-#### ROUTES TO ADD STUFF TO THE DATABASE ####
+#### ROUTES TO ADD / READ STUFF TO THE DATABASE ####
 
-# Route to add a new player
-# URL https://skelly-game-analytics-bab21bc24913.herokuapp.com/add_player_json
-# Expected JSON format: {"username": "user123", "email": "email@example.com"}
-@app.route('/add_player_json', methods=['POST'])
+# Route to add a new player (POST) and retrieve all players (GET)
+@app.route('/add_player_json', methods=['GET', 'POST'])
 def add_player_json():
-    data = request.json
-    new_player = Player(username=data['username'], email=data['email'])
-    db.session.add(new_player)
-    db.session.commit()
-    return jsonify({"message": "Player added successfully"}), 201
+    """
+    Add a new player to the database (POST) if the username doesn't exist, or
+    retrieve all players (GET).
 
-# Route to submit a score
-# URL https://skelly-game-analytics-bab21bc24913.herokuapp.com/submit_score_json
-# Expected JSON format: {"player_id": 1, "score": 100}
-@app.route('/submit_score_json', methods=['POST'])
+    Expected JSON format for POST:
+    {
+        "username": "user123"
+    }
+
+    Response JSON format for POST:
+    If the username doesn't exist:
+    {
+        "message": "Player added successfully"
+    }
+    If the username already exists:
+    {
+        "error": "Username already exists"
+    }
+
+    Response JSON format for GET:
+    [
+        {
+            "player_id": 1,
+            "username": "user123"
+        },
+        {
+            "player_id": 2,
+            "username": "another_user"
+        },
+        # ... (more players)
+    ]
+    """
+    if request.method == 'POST':
+        data = request.json
+        username = data.get('username', '').strip()
+        
+        # Check if the username already exists
+        existing_player = Player.query.filter_by(username=username).first()
+        if existing_player:
+            return jsonify({"error": "Username already exists"}), 400
+        
+        new_player = Player(username=username)
+        db.session.add(new_player)
+        db.session.commit()
+        return jsonify({"message": "Player added successfully"}), 201
+    elif request.method == 'GET':
+        players = Player.query.all()
+        player_list = [
+            {
+                "player_id": player.player_id,
+                "username": player.username
+            }
+            for player in players
+        ]
+        return jsonify(player_list)
+
+# Route to submit a score (POST) and retrieve all scores (GET)
+@app.route('/submit_score_json', methods=['GET', 'POST'])
 def submit_score_json():
-    data = request.json
-    new_score = Highscore(player_id=data['player_id'], score=data['score'])
-    db.session.add(new_score)
-    db.session.commit()
-    return jsonify({"message": "Score submitted successfully"}), 201
+    """
+    Submit a player's score (POST) or retrieve all scores (GET).
 
-# Route to update time played
-# URL https://skelly-game-analytics-bab21bc24913.herokuapp.com/update_time_played_json
-# Expected JSON format: {"player_id": 1, "time_played": 3600}
-@app.route('/update_time_played_json', methods=['POST'])
+    Expected JSON format for POST:
+    {
+        "player_id": 1,
+        "score": 100
+    }
+
+    Response JSON format for POST:
+    {
+        "message": "Score submitted successfully"
+    }
+
+    Response JSON format for GET:
+    [
+        {
+            "score_id": 1,
+            "player_id": 1,
+            "score": 100
+        },
+        {
+            "score_id": 2,
+            "player_id": 2,
+            "score": 150
+        },
+        # ... (more scores)
+    ]
+    """
+    if request.method == 'POST':
+        data = request.json
+        player_id = data.get('player_id', None)
+        score = data.get('score', None)
+
+        # Check if player_id is provided and exists
+        if player_id is None or Player.query.get(player_id) is None:
+            return jsonify({"error": "Invalid player_id"}), 400
+
+        # Check if score is provided
+        if score is None:
+            return jsonify({"error": "Score is required"}), 400
+
+        new_score = Highscore(player_id=player_id, score=score)
+        db.session.add(new_score)
+        db.session.commit()
+        return jsonify({"message": "Score submitted successfully"}), 201
+    elif request.method == 'GET':
+        scores = Highscore.query.all()
+        score_list = [
+            {
+                "score_id": score.score_id,
+                "player_id": score.player_id,
+                "score": score.score
+            }
+            for score in scores
+        ]
+        return jsonify(score_list)
+
+# Route to update time played (POST) and retrieve all players with time played (GET)
+@app.route('/update_time_played_json', methods=['GET', 'POST'])
 def update_time_played_json():
-    data = request.json
-    player = Player.query.get(data['player_id'])
-    if player:
-        player.time_played += timedelta(seconds=data['time_played'])
-        db.session.commit()
-        return jsonify({"message": "Time updated successfully"}), 200
-    return jsonify({"error": "Player not found"}), 404
+    """
+    Update a player's time played (POST) or retrieve all players with time played (GET).
 
-@app.route('/submit_number', methods=['POST'])
-def submit_number():
-    try:
-        data = request.get_json()
-        print("Received data:", data)  # Log the received data
-        new_number = NumberEntry(number=data['number'])
-        db.session.add(new_number)
-        db.session.commit()
-        return {"message": "Number added successfully"}, 200
-    except KeyError as e:
-        print("KeyError:", e)
-        return {"error": f"Missing key: {e}"}, 400
-    except Exception as e:
-        print("Error:", e)
-        return {"error": str(e)}, 500
+    Expected JSON format for POST:
+    {
+        "player_id": 1,
+        "time_played": 3600
+    }
+
+    Response JSON format for POST:
+    {
+        "message": "Time updated successfully"
+    }
+
+    Response JSON format for GET:
+    [
+        {
+            "player_id": 1,
+            "username": "user123",
+            "time_played": 3600.0
+        },
+        {
+            "player_id": 2,
+            "username": "another_user",
+            "time_played": 7200.0
+        },
+        # ... (more players with time played)
+    ]
+    """
+    if request.method == 'POST':
+        data = request.json
+        player_id = data.get('player_id', None)
+        time_played = data.get('time_played', None)
+
+        # Check if player_id is provided and exists
+        if player_id is None or Player.query.get(player_id) is None:
+            return jsonify({"error": "Invalid player_id"}), 400
+
+        # Check if time_played is provided
+        if time_played is None:
+            return jsonify({"error": "Time played is required"}), 400
+
+        player = Player.query.get(player_id)
+        if player:
+            player.time_played += timedelta(seconds=time_played)
+            db.session.commit()
+            return jsonify({"message": "Time updated successfully"}), 200
+        return jsonify({"error": "Player not found"}), 404
+    elif request.method == 'GET':
+        players = Player.query.all()
+        player_list = [
+            {
+                "player_id": player.player_id,
+                "username": player.username,
+                "time_played": player.time_played.total_seconds()
+            }
+            for player in players
+        ]
+        return jsonify(player_list)
+
+# Route to submit a number (POST) and retrieve all numbers (GET)
+@app.route('/submit_number_json', methods=['GET', 'POST'])
+def submit_number_json():
+    """
+    Submit a number (POST) or retrieve all numbers (GET).
+
+    Expected JSON format for POST:
+    {
+        "number": 42
+    }
+
+    Response JSON format for POST:
+    {
+        "message": "Number added successfully"
+    }
+
+    Response JSON format for GET:
+    [
+        {
+            "id": 1,
+            "number": 42
+        },
+        {
+            "id": 2,
+            "number": 123
+        },
+        # ... (more numbers)
+    ]
+    """
+    if request.method == 'POST':
+        try:
+            data = request.json
+            new_number = NumberEntry(number=data['number'])
+            db.session.add(new_number)
+            db.session.commit()
+            return {"message": "Number added successfully"}, 200
+        except KeyError as e:
+            return {"error": f"Missing key: {e}"}, 400
+    elif request.method == 'GET':
+        numbers = NumberEntry.query.all()
+        number_list = [
+            {
+                "id": number.id,
+                "number": number.number
+            }
+            for number in numbers
+        ]
+        return jsonify(number_list)
 
 # Add other routes as needed...
 
