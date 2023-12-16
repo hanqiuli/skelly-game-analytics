f5aea951fb51d4178560d27f6b2ab57abfdc2049
hanqiuli <hanqiulicai@gmail.com>
2023-12-16 18:29:13 +0100

    attempt to fix error

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn web_app:app
+web: gunicorn new_app:app2
--- new_app.py
+++ new_app.py
@@ -3,16 +3,16 @@ from flask_migrate import Migrate
 from datetime import datetime
 import json
 
-app = Flask(__name__)
+app2 = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fwsbrmejpihtpr:63694976e4b5c59af5029d3802c625c2ab89828c15f55049f86fe58156f744e9@ec2-34-250-252-161.eu-west-1.compute.amazonaws.com:5432/d3m1dq8b7u0ors'
+app2.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://fwsbrmejpihtpr:63694976e4b5c59af5029d3802c625c2ab89828c15f55049f86fe58156f744e9@ec2-34-250-252-161.eu-west-1.compute.amazonaws.com:5432/d3m1dq8b7u0ors'
 
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy(app2)
+migrate = Migrate(app2, db)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app2.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app2)
 class GameStats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.JSON)  # Stores the JSON data of game stats
@@ -25,7 +25,7 @@ class GameStats(db.Model):
             'timestamp': self.timestamp.isoformat()
         }
 
-@app.route('/save_game_data', methods=['POST'])
+@app2.route('/save_game_data', methods=['POST'])
 def save_game_data():
     data = request.json
     new_stats = GameStats(data=data)
@@ -33,7 +33,7 @@ def save_game_data():
     db.session.commit()
     return jsonify({'message': 'Game data saved successfully', 'id': new_stats.id}), 201
 
-@app.route('/load_game_data', methods=['GET'])
+@app2.route('/load_game_data', methods=['GET'])
 def load_game_data():
     latest_stats = GameStats.query.order_by(GameStats.timestamp.desc()).first()
     if latest_stats:
@@ -41,11 +41,11 @@ def load_game_data():
     else:
         return jsonify({'error': 'No game data found'}), 404
 
-@app.route('/view_data')
+@app2.route('/view_data')
 def view_data():
     game_stats = GameStats.query.order_by(GameStats.timestamp.desc()).all()
     return render_template('view_data.html', game_stats=game_stats)
 
 if __name__ == '__main__':
     db.create_all()
-    app.run(debug=True)
+    app2.run(debug=True)
