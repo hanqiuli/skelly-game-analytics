bc6355e468ec2b8c32a3a36d94da738c72a40f36
hanqiuli <hanqiulicai@gmail.com>
2023-12-17 15:16:33 +0100

    updated fields for sql

--- new_app.py
+++ new_app.py
@@ -13,20 +13,85 @@ migrate = Migrate(app, db)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 class GameStats(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.JSON)  # Stores the JSON data of game stats
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    isGameOver = db.Column(db.Boolean, default=False)
+    skellyDeaths = db.Column(db.Integer, default=0)
+    enemiesKilled = db.Column(db.Integer, default=0)
+    damageDone = db.Column(db.Integer, default=0)
+    damageTaken = db.Column(db.Integer, default=0)
+    heavyAttacksUsed = db.Column(db.Integer, default=0)
+    lightAttacksUsed = db.Column(db.Integer, default=0)
+    dashesUsed = db.Column(db.Integer, default=0)
+    distanceTraveled = db.Column(db.Float, default=0.0)
+    timeElapsed = db.Column(db.Float, default=0.0)
+    mementosCollected = db.Column(db.Integer, default=0)
+    levelReached = db.Column(db.Integer, default=0)
+    upgradesMilkCollected = db.Column(db.Integer, default=0)
+    skullHammerPickedUpJoe = db.Column(db.Boolean, default=False)
+    ribberanPickedUpJane = db.Column(db.Boolean, default=False)
+    boneSwordFabulaPickedUp = db.Column(db.Boolean, default=False)
+    weaponEquipped = db.Column(db.Integer, default=1)
+    currentLevel = db.Column(db.Integer, default=0)
+    playerXPosition = db.Column(db.Float, default=0.0)
+    playerYPosition = db.Column(db.Float, default=0.0)
+    playerZPosition = db.Column(db.Float, default=0.0)
+    healthAtSaveTime = db.Column(db.Integer, default=0)
 
     def to_dict(self):
         return {
             'id': self.id,
-            'data': self.data,
+            'isGameOver': self.isGameOver,
+            'skellyDeaths': self.skellyDeaths,
+            'enemiesKilled': self.enemiesKilled,
+            'damageDone': self.damageDone,
+            'damageTaken': self.damageTaken,
+            'heavyAttacksUsed': self.heavyAttacksUsed,
+            'lightAttacksUsed': self.lightAttacksUsed,
+            'dashesUsed': self.dashesUsed,
+            'distanceTraveled': self.distanceTraveled,
+            'timeElapsed': self.timeElapsed,
+            'mementosCollected': self.mementosCollected,
+            'levelReached': self.levelReached,
+            'upgradesMilkCollected': self.upgradesMilkCollected,
+            'skullHammerPickedUpJoe': self.skullHammerPickedUpJoe,
+            'ribberanPickedUpJane': self.ribberanPickedUpJane,
+            'boneSwordFabulaPickedUp': self.boneSwordFabulaPickedUp,
+            'weaponEquipped': self.weaponEquipped,
+            'currentLevel': self.currentLevel,
+            'playerXPosition': self.playerXPosition,
+            'playerYPosition': self.playerYPosition,
+            'playerZPosition': self.playerZPosition,
+            'healthAtSaveTime': self.healthAtSaveTime,
             'timestamp': self.timestamp.isoformat()
         }
 
 @app.route('/save_game_data', methods=['POST'])
 def save_game_data():
     data = request.json
-    new_stats = GameStats(data=data)
+    new_stats = GameStats(
+        isGameOver=data.get('isGameOver', False),
+        skellyDeaths=data.get('skellyDeaths', 0),
+        enemiesKilled=data.get('enemiesKilled', 0),
+        damageDone=data.get('damageDone', 0),
+        damageTaken=data.get('damageTaken', 0),
+        heavyAttacksUsed=data.get('heavyAttacksUsed', 0),
+        lightAttacksUsed=data.get('lightAttacksUsed', 0),
+        dashesUsed=data.get('dashesUsed', 0),
+        distanceTraveled=data.get('distanceTraveled', 0.0),
+        timeElapsed=data.get('timeElapsed', 0.0),
+        mementosCollected=data.get('mementosCollected', 0),
+        levelReached=data.get('levelReached', 0),
+        upgradesMilkCollected=data.get('upgradesMilkCollected', 0),
+        skullHammerPickedUpJoe=data.get('skullHammerPickedUpJoe', False),
+        ribberanPickedUpJane=data.get('ribberanPickedUpJane', False),
+        boneSwordFabulaPickedUp=data.get('boneSwordFabulaPickedUp', False),
+        weaponEquipped=data.get('weaponEquipped', 1),
+        currentLevel=data.get('currentLevel', 0),
+        playerXPosition=data.get('playerXPosition', 0.0),
+        playerYPosition=data.get('playerYPosition', 0.0),
+        playerZPosition=data.get('playerZPosition', 0.0),
+        healthAtSaveTime=data.get('healthAtSaveTime', 0)
+    )
     db.session.add(new_stats)
     db.session.commit()
     return jsonify({'message': 'Game data saved successfully', 'id': new_stats.id}), 201
@@ -45,5 +110,4 @@ def view_data():
     return render_template('view_data.html', game_stats=game_stats)
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
